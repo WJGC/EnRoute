590675f5a2a0032a592793dbfc5967583d9aed8b
Kyle Sit <ktsit@ucsd.edu>
2018-02-15 17:56:07 -0800

    meetingsjoined.json reference removed to deploy

--- routes/meetingsJoined.js
+++ routes/meetingsJoined.js
@@ -3,9 +3,9 @@
  * GET home page.
  */
 
-var meetingsJoined = require("../meetingsJoined.json");
+//var meetingsJoined = require("../meetingsJoined.json");
 
 exports.meetingsJoined = function(req, res){
-  res.render('meetingsJoined', meetingsJoined);
+  //res.render('meetingsJoined', meetingsJoined);
   //res.render('meetings', meetingsJoined);
 };
