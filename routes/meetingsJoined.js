910b69776fee3e07e51508a11784596f9e93b1c6
Kyle Sit <ktsit@ucsd.edu>
2018-02-15 18:20:41 -0800

    meetingsJoined editted

--- routes/meetingsJoined.js
+++ routes/meetingsJoined.js
@@ -6,6 +6,15 @@
 //var meetingsJoined = require("../meetingsJoined.json");
 
 exports.meetingsJoined = function(req, res){
-  //res.render('meetingsJoined', meetingsJoined);
+  res.render('meetingsJoined', {	"meetingsJoined": [
+		{
+			"meetingLocation": "string",
+			"altitude": " alt ",
+			"longitude": "long",
+			"meetingName": "Geisel",
+			"time": "2:00",
+			"finished" : "false"			
+		}
+	]}
   //res.render('meetings', meetingsJoined);
 };
