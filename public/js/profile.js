929c9101e12e87b52de2c8cef1c0dd8b93a8325d
Kyle Sit <ktsit@ucsd.edu>
2018-02-22 21:12:24 -0800

    logout

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,3 +4,9 @@ var pic = sessionStorage.getItem('profPic');
 
 document.getElementById("ProfilePic").src = pic;
 document.getElementById("name").innerHTML = name;
+
+document.getElementById("logout").addEventListener("click",function(){
+  window.location.href = "/";
+  sessionStorage.setItem('profPic','');
+  sessionStorage.setItem('user','');
+});
